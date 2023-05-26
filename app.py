fc8b3784c1a4eb27977dadb0e462db7c756c9fb7
ZeYiLin <944270057@qq.com>
2023-05-26 08:56:08 +0800

    Update app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import gradio as gr
+from paddleocr_v3_inference import inferenceOCR
+import cv2
+
+def inference(input_image):
+    result_image, return_json = inferenceOCR(input_image)
+    return cv2.cvtColor(result_image, cv2.COLOR_BGR2RGB), return_json
+
+if __name__ == "__main__":
+    gr.Interface(
+        fn=inference,
+        inputs="image",
+        outputs=["image", gr.JSON()]).launch()
