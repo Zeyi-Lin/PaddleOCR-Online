83176908be0a0441adc44463510f3a0045efe351
ZeYiLin <944270057@qq.com>
2023-05-26 01:16:44 +0000

    Edit app.py

--- app.py
+++ app.py
@@ -11,4 +11,24 @@ if __name__ == "__main__":
         title="PaddleOCR_PyTorch",
         fn=inference,
         inputs="image",
-        outputs=["image", gr.JSON()]).launch()
+        outputs=["image", gr.JSON()],
+import gradio as gr
+from paddleocr_v3_inference import inferenceOCR
+import cv2
+
+def inference(input_image):
+    result_image, return_json = inferenceOCR(input_image)
+    return cv2.cvtColor(result_image, cv2.COLOR_BGR2RGB), return_json
+
+if __name__ == "__main__":
+    gr.Interface(
+        fn=inference,
+        inputs="image",
+        outputs=["image", gr.JSON()],
+        examples=["./images/01.jpg",
+                  "./images/02.jpg",
+                  "./images/03.jpg",
+                  "./images/04.jpg",
+                  "./images/05.jpg"]).launch()
+
+).launch()
